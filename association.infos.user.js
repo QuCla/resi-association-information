fc1c4587f0457f1d21b1437771a310ce7725ab7c
QuCla <122170839+QuCla@users.noreply.github.com>
2023-02-09 05:24:12 +0100

    replaced brand with VName

--- association.infos.user.js
+++ association.infos.user.js
@@ -43,8 +43,10 @@ function readAssociation(){
         success : function(r) {
             //place association name in header
             VName = r.associationName.toLocaleString();
-            document.getElementsByClassName('brand')[0].after(Einschub);
-            Einschub.innerHTML = VName;
+            //document.getElementsByClassName('brand')[0].after(Einschub);
+            //Einschub.innerHTML = VName;
+            let brand = document.getElementsByClassName('brand')[0];
+            brand.innerText = VName;
 
             //get values from API
             VAnzahl = r.associationUsers.length;
